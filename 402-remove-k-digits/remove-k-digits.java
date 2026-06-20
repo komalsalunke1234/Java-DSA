bdef9c7a0e63cd4714982bdba0bb0a64d2b62559
komal kiran salunke <153985795+komalsalunke1234@users.noreply.github.com>
2026-06-20 20:46:20 +0530

    Time: 9 ms (92.88%) | Memory: 46.7 MB (73.71%) - LeetSync

---
+++ 402-remove-k-digits/remove-k-digits.java
@@ -0,0 +1,32 @@
+class Solution {
+    public String removeKdigits(String num, int k) {
+
+        StringBuilder sb = new StringBuilder();
+
+        for (char digit : num.toCharArray()) {
+
+            while (k > 0 &&
+                   sb.length() > 0 &&
+                   sb.charAt(sb.length() - 1) > digit) {
+
+                sb.deleteCharAt(sb.length() - 1);
+                k--;
+            }
+
+            sb.append(digit);
+        }
+
+        // Remove remaining digits from end
+        while (k > 0 && sb.length() > 0) {
+            sb.deleteCharAt(sb.length() - 1);
+            k--;
+        }
+
+        // Remove leading zeros
+        while (sb.length() > 0 && sb.charAt(0) == '0') {
+            sb.deleteCharAt(0);
+        }
+
+        return sb.length() == 0 ? "0" : sb.toString();
+    }
+}
