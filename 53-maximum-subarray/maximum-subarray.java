dc2b7d8a2c4ff987c146f83ef5c4c46236547429
komal kiran salunke <153985795+komalsalunke1234@users.noreply.github.com>
2026-06-28 18:43:01 +0530

    Time: 1 ms (99.95%) | Memory: 77.4 MB (31.71%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -18,5 +18,17 @@ class Solution {
             maxSum=Math.max(maxSum,currentSum);
         }
         return maxSum;
+
+
+        // int maxSum=Integer.MIN_VALUE;
+        // for(int i=0;i<nums.length;i++){
+        //     int sum=0;
+        //     for(int j=i;j<nums.length;j++){
+        //         sum+=nums[j];
+        //         maxSum=Math.max(maxSum,sum);
+
+        //     }
+        // }
+        // return maxSum;
     }
 }
