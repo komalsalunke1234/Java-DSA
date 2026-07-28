70d724b4592c9d010c639dbc5077c2167c27f2c0
komal kiran salunke <153985795+komalsalunke1234@users.noreply.github.com>
2026-07-28 23:25:15 +0530

    Time: 32 ms (53.82%) | Memory: 48.4 MB (23.69%) - LeetSync

---
+++ 3812-smallest-palindromic-rearrangement-i/smallest-palindromic-rearrangement-i.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String smallestPalindrome(String s) {
+        char[] arr = s.toCharArray();
+        int n = arr.length;
+        int half = n / 2;
+
+        
+        Arrays.sort(arr, 0, half);
+
+        for (int i = 0; i < half; i++) {
+            arr[n - 1 - i] = arr[i];
+        }
+
+        return new String(arr);
+    }
+}
