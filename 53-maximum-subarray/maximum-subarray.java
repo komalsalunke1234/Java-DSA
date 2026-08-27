91734dd4a869b225b6729d38ca810f73ee1b0116
komal kiran salunke <153985795+komalsalunke1234@users.noreply.github.com>
2026-08-27 12:04:13 +0530

    Time: 1 ms (99.95%) | Memory: 77.3 MB (48.99%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.java
+++ 53-maximum-subarray/maximum-subarray.java
@@ -1,34 +1,18 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        // int sum=0;
-        // int n=nums.length;
-        // int maxi=Integer.MIN_VALUE;
-        // for(int i=0;i<n;i++){
-        //     for(int j=0;j<n;j++){
-        //         sum+=nums[i]+nums[j];
-        //         return Math.max(sum);
-        //     }
-        // }
-
-
-        int currentSum=nums[0];
-        int maxSum=nums[0];
+        int maxsum=nums[0];
+        int currentsum=nums[0];
         for(int i=1;i<nums.length;i++){
-            currentSum=Math.max(nums[i],currentSum+nums[i]);
-            maxSum=Math.max(maxSum,currentSum);
+            if(currentsum+nums[i]>nums[i])
+            {
+                currentsum+=nums[i];
+
+            }
+            else{
+                currentsum=nums[i];
+            }
+            maxsum=Math.max(currentsum,maxsum);
         }
-        return maxSum;
-
-
-        // int maxSum=Integer.MIN_VALUE;
-        // for(int i=0;i<nums.length;i++){
-        //     int sum=0;
-        //     for(int j=i;j<nums.length;j++){
-        //         sum+=nums[j];
-        //         maxSum=Math.max(maxSum,sum);
-
-        //     }
-        // }
-        // return maxSum;
+        return maxsum;
     }
 }
